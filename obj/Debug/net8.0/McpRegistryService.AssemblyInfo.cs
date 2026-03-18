a2a81f68fc38b012f0216265f07b4deac18f260d
cgfph <fabian.hanggi@vancouver.ca>
2026-03-18 11:19:47 -0700

    preparation to host on "render"

--- obj/Debug/net8.0/McpRegistryService.AssemblyInfo.cs
+++ obj/Debug/net8.0/McpRegistryService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("McpRegistryService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0beabb5e973bf123ce88559a01e1c45ff199500b")]
 [assembly: System.Reflection.AssemblyProductAttribute("McpRegistryService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("McpRegistryService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
